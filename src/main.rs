af0ddc0a901d39ebbb2eaac53d89ffc29581ed8c
Alex Kiernan <alex.kiernan@gmail.com>
2023-12-28 14:36:43 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+use figlet_rs::FIGfont;
+use hello_lib::greet;
+
+fn main() {
+    let standard_font = FIGfont::standard().unwrap();
+    let greeting = greet();
+    let figure = standard_font.convert(&greeting);
+    println!("{}", figure.unwrap());
+}
